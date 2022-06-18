645882270af0ca1b50fd5bebb887ede2f5627071
seax999 <yhx15779081105@163.com>
2022-06-18 23:29:13 +0800

    dosc: utils 修改

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,3 +1,8 @@
-export const isPhone = ()=>{
-    return /Android|iPhone|iPad|iPod/i.test(window.navigator.userAgent)
-}
+export const isPhone = () => {
+    return /Android|iPhone|iPad|iPod/i.test(window.navigator.userAgent);
+};
+
+export const remAndFontSize = () => {
+    document.getElementsByTagName("html")[0].style.fontSize = `${document.body.clientWidth / 7.5}px`;
+    document.body.style.fontSize = "16px";
+};
